4cd6a1d9f1b9a6c09877cda4ba872cbc5b1e06d7
Erick Souza <ericknilsen@gmail.com>
2018-07-14 10:24:01 -0300

    Configuração de CORS

--- src/main/java/com/tcc/inspetor/spring/Main.java
+++ src/main/java/com/tcc/inspetor/spring/Main.java
@@ -2,6 +2,10 @@ package com.tcc.inspetor.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class Main {
@@ -11,5 +15,15 @@ public class Main {
 		SpringApplication.run(Main.class, args);
 
 	}
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+            	registry.addMapping("/**");
+            }
+        };
+    }
 
 }
